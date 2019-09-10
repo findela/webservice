70b97ba36a0f803a3551c912a3a0d426ff9d4b72
Tamaghna Banerjee <tamaghna@collegify.com>
2019-09-11 00:04:07 +0530

    Customer login and registration added

--- controller/user.js
+++ controller/user.js
@@ -14,12 +14,82 @@ router.post("/register", (req, res, next) => {
         req.body.userType,
         req.body.status
     );
-    db.query(locator.getAddUserSQL(), (err, data)=> {
-        console.log(err);
-        res.status(200).json({
-            message:"New user added",
-            data: data.insertId
-        });
+
+    db.query(locator.checkUserExistSQL(), (err, data)=> {
+        if(!err) {
+            if (data[0].userCount > 0) {
+                res.status(302).json({
+                    message: "Ahhh! Same email id already exist!",
+                    data: data[0],
+                    status: 302
+                });
+            }
+            else {
+                db.query(locator.getAddUserSQL(), (err, data) => {
+                    res.status(201).json({
+                        message: "Voila! Registration successful",
+                        data: {
+                            userId: data.insertId
+                        },
+                        status: 201
+                    });
+                });
+            }
+        }
+        else {
+            res.status(500).json({
+                message: "Shhh! Internal server error",
+                status: 500
+            });
+        }
+    });
+});
+
+router.post("/login", (req, res, next) => {
+    //read product information from request
+    let locator = new User(
+        req.body.emailId,
+        req.body.password
+    );
+
+    db.query(locator.checkUserAuthSQL(req.body.emailId,req.body.password), (err, data)=> {
+        if(!err) {
+            if (data[0].userCount == 1) {
+                db.query(locator.fetchUserDetailsSQL(req.body.emailId), (err, data) => {
+                    if(!err) {
+                        res.status(202).json({
+                            message: "Great! Login successful.",
+                            data: {
+                                firstName: data[0].first_name,
+                                lastName: data[0].last_name,
+                                emailId: data[0].email,
+                                userType: data[0].user_type
+                            },
+                            status: 202
+                        });
+                    }
+                    else {
+                        res.status(500).json({
+                            message: "Shhh! Internal server error",
+                            status: 500
+                        });
+                    }
+                });
+            }
+            else {
+                res.status(302).json({
+                    message: "Ohhh! Credentials mismatched!",
+                    data: data[0],
+                    status: 302
+                });
+            }
+        }
+        else {
+            res.status(500).json({
+                message: "Shhh! Internal server error",
+                status: 500
+            });
+        }
     });
 });
 
--- model/user.js
+++ model/user.js
@@ -9,10 +9,27 @@ class User {
         this.status=status;
     }
 
+    checkUserExistSQL() {
+        let sql = `SELECT COUNT(*) as userCount FROM users where users.email LIKE '%${this.emailId}%'`;
+        return sql;
+    }
+
     getAddUserSQL() {
         let sql = `INSERT INTO users(first_name,last_name,email,password,user_type,status) VALUES('${this.firstName}','${this.lastName}','${this.emailId}','${this.password}','${this.userType}',${this.status})`;
         return sql;
     }
+
+    checkUserAuthSQL(email,password) {
+        let sql = `SELECT COUNT(*) as userCount FROM users where users.email = '${email}' AND users.password = '${password}'`;
+        return sql;
+    }
+
+    fetchUserDetailsSQL(email) {
+        let sql = `SELECT * FROM users where users.email = '${email}'`;
+        return sql;
+    }
+
+
 }
 
 export default User;
